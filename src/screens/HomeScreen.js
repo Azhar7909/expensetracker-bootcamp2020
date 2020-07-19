5df06d792f5c77bcb1afd458585d6b6203f5210d
Azhar7909 <muhammadazharhussain54@gmail.com>
2020-07-19 15:09:25 +0500

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function HomeScreen() {
     return (
         <div>
+            <Header />
             Home Screen
+            <Footer />
         </div>
     )
 }
