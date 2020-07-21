29aca0327e8296ac9c6c840aa3e169b68030858b
Azhar7909 <muhammadazharhussain54@gmail.com>
2020-07-21 09:28:54 +0500

    update

---
+++ src/screens/store/actions/AddTransactionAction.js
@@ -0,0 +1,17 @@
+// import { useReducer } from "react";
+// import TransactionReducer from "../reducers/TransactionReducer";
+// import { initialState } from "../store";
+
+
+//     let [state, dispatch] = useReducer(TransactionReducer, initialState);
+
+// function addTransaction(transObj) {
+//     dispatch({
+//         type: 'ADD_TRANSACTION',
+//         payload: {
+//             desc: transObj.desc,
+//             amount: transObj.amount
+//         }
+//     })
+// }
+// export default addTransaction;
--- src/screens/store/store.js
+++ src/screens/store/store.js
@@ -1,7 +1,9 @@
 import { createContext } from "react";
 
 let initialSate = [
-    {desc: "Book", amount: 800},
+    {desc: "Cash", amount: 800},
+    {desc: "Cash", amount: -800},
+    {desc: "Cash", amount: 800},
     {desc: "Cash", amount: -800},
 ]
 
